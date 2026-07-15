7410e86e595a82315f42133c8fe4fe2a80c72aa6
BicBoiTaco83337 <bicboitaco83337@gmail.com>
2026-07-15 09:56:51 -0400

    prints image of randome pokemon chosen from first 1000 pokemon in database

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,32 @@ function App(){
 
   const [image, setImage] = useState(null);
 
+  const imageStyle = {
+
+  }
+
   
-  // async function getPokeData(){
-  //   const response = await axios.get("https://pokeapi.co/api/v2/pokemon/totodile") 
-  //  return response
-  // }
+  //  async function fetchPokemon(){
+  //   const response = await axios.get("https://pokeapi.co/api/v2/pokemon") 
+  //   return response
+  //  }
 
   async function handleClick(){
-    const pokemon = await axios.get("https://pokeapi.co/api/v2/pokemon")
-    const randNum = Math.random(0,pokemonData.length)
-    setImage(pokemon["data"]["sprites"]["front_default"])
+    const randNum = Math.floor(Math.random() * 1000)
+    console.log("randomNum = ", randNum)
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randNum}`)
+      .then(response => {
+        console.log("response = ", response)
+        const pokemonObj = response["data"]
+        console.log("pokemon List=", pokemonObj)
+        const firstPokeImageName = Object.keys(pokemonObj["sprites"])[4]
+        console.log(firstPokeImageName)
+        setImage(pokemonObj["sprites"][firstPokeImageName])
+        
+      })
+      .catch(error => {
+        console.log("Error:", error)
+      })  
   } 
 
   return(
@@ -26,7 +42,7 @@ function App(){
     <button onClick={handleClick}>create Pokemon</button>
   </div>
   <div>
-   <img src={image} alt={"pokemon"}/>
+   <img src={image} alt={"pokemon"} width="500" height="600"/>
   </div>
   </>
     
